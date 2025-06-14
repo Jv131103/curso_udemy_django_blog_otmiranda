a8315fddf7eb3e3eda6a538a7104822afb313b93
Jv131103 <jjv1792@gmail.com>
2025-06-14 11:27:57 -0300

    feat/preparativos para projeto

---
+++ iniciar_env.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# set -x  # Ativa o modo debug, mostrando os comandos executados
+
+echo "Iniciando o ambiente virtual"
+
+source env/bin/activate
+
+echo "Fim! Ambiente gerado com sucesso!"
