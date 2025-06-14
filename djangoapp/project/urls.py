84e52d2f76cfbd7fc42f0077342a3647dadac184
Jv131103 <jjv1792@gmail.com>
2025-06-14 14:49:45 -0300

    update/atualizacao 14/06/2025

--- djangoapp/project/urls.py
+++ djangoapp/project/urls.py
@@ -1,3 +1,4 @@
+# type: ignore
 """
 URL configuration for project project.
 
@@ -14,9 +15,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+
+# Isso é importante, por que senão, não conseguimos ver os arquivos de media
+# enviados pelo nosso usuário em nosso desenvolvimento
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
