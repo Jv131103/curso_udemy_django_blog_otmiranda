f0bf0cc629cc181e79f65b51f48e10282b552cff
Jv131103 <jjv1792@gmail.com>
2025-07-11 11:51:09 -0300

    update/atualizacao 11/07/2025

--- djangoapp/site_setup/models.py
+++ djangoapp/site_setup/models.py
@@ -1,4 +1,8 @@
+from typing import Iterable
+
 from django.db import models
+from utils.images import resize_image
+from utils.model_validators import validate_png
 
 
 # Create your models here.
@@ -39,8 +43,18 @@ class SiteSetup(models.Model):
     faveicon = models.ImageField(
         upload_to='assets/favicon/%Y/%m/',
         blank=True,
-        default=""
+        default="",
+        validators=[validate_png]
     )
+
+    def save(self, *args, **kwargs):
+        current_favicon_name = str(self.faveicon.name)
+        super().save(*args, **kwargs)
+        favicon_changed = False
+        if self.faveicon:
+            favicon_changed = current_favicon_name != self.faveicon.name
+        if favicon_changed:
+            resize_image(self.faveicon, 32)
 
     def __str__(self):
         return self.title
